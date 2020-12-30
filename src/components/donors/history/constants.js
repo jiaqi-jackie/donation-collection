c7406c141c73c8b28d3e61547e7a40251ec5995f
jiaqi-jackie <jiaqili2001023@gmail.com>
2020-12-30 10:07:06 -0600

    donors

---
+++ src/components/donors/history/constants.js
@@ -0,0 +1,10 @@
+export const COMPLETED = "completed";
+export const PENDING = "pending";
+export const SCHEDULED = "scheduled";
+export const ALL = "all";
+
+export const COMPLETE_MSG = "Completed!";
+export const SCHEDULED_MSG = "Scheduled On";
+export const PENDING_MSG = "Pending...";
+
+export const URL_DONOR_GET_ALL_DONATIONS = "";
